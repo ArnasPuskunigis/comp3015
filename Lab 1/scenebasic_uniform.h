b67e50050e4eedb04c95ea0a3202aa699bc5be31
Arnas Puskunigis <arnaspusk204@gmail.com>
2025-03-01 21:03:15 +0000

    teapot on a plane with a light and blinnphong added

--- Lab 1/scenebasic_uniform.h
+++ Lab 1/scenebasic_uniform.h
@@ -8,21 +8,26 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include "helper/torus.h"
+#include "helper/teapot.h"
+#include "glm/glm.hpp"
+#include "helper/plane.h"
+#include "helper/objmesh.h"
 
 class SceneBasic_Uniform : public Scene
 {
 private:
-    GLuint vaoHandle;
-    GLSLProgram prog;
-    float angle;
+    Plane plane;
+    Teapot teapot;
 
+    GLSLProgram prog;
+    void setMatrices();
     void compile();
 
 public:
     SceneBasic_Uniform();
 
     void initScene();
-    void update( float t );
+    void update(float t);
     void render();
     void resize(int, int);
 };
